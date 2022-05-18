b0bed82e0dee9d0e2cc525c17c2570a6bbd009c5
santdev404 <santdev404@gmail.com>
2022-05-17 21:00:28 -0500

    Basic routing

---
+++ src/app/Router.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Exceptions\RouteNotFoundException;
+
+class Router{
+
+    private array $routes;
+
+    public function register(string $route, callable $action): self
+    {
+        $this->routes[$route] = $action;
+        return $this;
+    }
+
+    public function resolve(string $requestUri)
+    {
+        $route = explode('?', $requestUri)[0];
+        $action = $this->routes[$route] ?? null;
+
+        if(!$action){
+            throw new RouteNotFoundException();
+        }
+
+        call_user_func($action);
+
+    }
+
+}
--- src/public/index.php
+++ src/public/index.php
@@ -3,54 +3,21 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-// varibale, anonymous & arrow function
+$router = new App\Router;
 
+$router->register(
+    '/', 
+    function () {
+        echo 'Home';
+    }
+);
 
-function sum(int|float ...$numbers): int|float{
-    return array_sum($numbers);
-}
-$x = 'sum';
+$router->register(
+    '/invoices', 
+    function () {
+        echo 'Invoices';
+    }
+);
 
+echo $router->resolve($_SERVER['REQUEST_URI']);
 
-if(is_callable($x)){
-    echo $x(1,2,3,4).PHP_EOL;
-}else{
-    echo 'Not callable';
-}
-
-
-//lambda
-$i = 9;
-$suma = function (int|float ...$numbers) use($i): int|float{
-    echo $i.PHP_EOL;
-    return array_sum($numbers);
-};
-
-echo $suma(4,5,6,7).PHP_EOL;
-
-//callable
-$array = [1,2,3,4];
-$array2 = array_map(function($element){
-    return $element * 2;
-}, $array);
-
-print_r($array).PHP_EOL;
-print_r($array2).PHP_EOL;
-
-
-echo "callable".PHP_EOL;
-$add = function(callable $callback, int|float ...$numbers): int|float{
-    return $callback(array_sum($numbers));
-};
-
-echo $add(function($element){
-    return $element * 2;
-}, 9,8,7,6).PHP_EOL;
-
-//arrow
-echo "arrow".PHP_EOL;
-$array = [1,2,4,5];
-
-$array2 = array_map(fn($number) => $number * $number, $array);
-
-print_r($array2);
